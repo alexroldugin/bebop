5ecabd30b4b4c656c933a8b694435a872ac07654
Alex Roldugin <2838600+alexroldugin@users.noreply.github.com>
2019-06-16 18:43:30 +0300

    added initial implementation of HomePage which is based on Popup.jsx container

---
+++ src/containers/HomePage.jsx
@@ -0,0 +1,44 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import PopupPage from '../components/PopupPage';
+import keySequence from '../key_sequences';
+import { commandOfSeq } from '../sagas/key_sequence';
+
+import {
+  makeSelectCandidatesItems,
+  makeSelectCandidatesIndex,
+  makeSelectScheme,
+  makeSelectMode,
+  makeSelectMarkedCandidateIds,
+  makeSelectSeparators,
+} from '../selectors/popup';
+
+export const mapStateToProps = createStructuredSelector({
+  candidates:         makeSelectCandidatesItems(),
+  index:              makeSelectCandidatesIndex(),
+  separators:         makeSelectSeparators(),
+  markedCandidateIds: makeSelectMarkedCandidateIds(),
+  mode:               makeSelectMode(),
+  scheme:             makeSelectScheme(),
+});
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    handleSelectCandidate: payload => dispatch({ type: 'SELECT_CANDIDATE', payload }),
+    handleInputChange:     payload => dispatch({ type: 'QUERY', payload }),
+    handleKeyDown:         (e) => {
+      const keySeq = keySequence(e);
+      if (commandOfSeq[keySeq]) {
+        e.preventDefault();
+        dispatch({ type: 'KEY_SEQUENCE', payload: keySeq });
+      }
+    },
+    dispatchQuit: () => {
+      dispatch({ type: 'POPUP_CLEANUP' });
+      dispatch({ type: 'POPUP_QUIT' });
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PopupPage);
---
+++ test/containers/HomePage.test.js
@@ -0,0 +1,182 @@
+import test from 'ava';
+import nisemono from 'nisemono';
+
+import {
+  mapStateToProps,
+  mapDispatchToProps,
+} from '../../src/containers/HomePage';
+
+const candidate = {
+  id:         'item_id',
+  label:      'Label',
+  type:       'tab',
+  args:       [],
+  faviconUrl: 'some://test.url',
+};
+
+test('<HomePage /> | props | with default state', (t) => {
+  t.deepEqual({
+    candidates:         [],
+    index:              0,
+    separators:         [],
+    markedCandidateIds: {},
+    mode:               '',
+    scheme:             {
+      enum: [],
+    },
+  }, mapStateToProps());
+});
+
+test('<HomePage /> | props | with default state specified directly', (t) => {
+  t.deepEqual({
+    candidates:         [],
+    index:              0,
+    separators:         [],
+    markedCandidateIds: {},
+    mode:               '',
+    scheme:             { enum: [] },
+  }, mapStateToProps({
+    popup: {
+      candidates: {
+        items: [],
+        index: 0,
+      },
+      separators:         [],
+      markedCandidateIds: {},
+      mode:               '',
+      scheme:             {
+        enum: [],
+      },
+    },
+  }));
+});
+
+test('<HomePage /> | props | with state with one candidate', (t) => {
+  t.deepEqual({
+    candidates:         [candidate],
+    index:              15,
+    separators:         [],
+    markedCandidateIds: {},
+    mode:               '',
+    scheme:             { enum: [] },
+  }, mapStateToProps({
+    popup: {
+      candidates: {
+        items: [candidate],
+        index: 15,
+      },
+      separators:         [],
+      markedCandidateIds: {},
+      mode:               '',
+      scheme:             {
+        enum: [],
+      },
+    },
+  }));
+});
+
+test('<HomePage /> | props | with state with two candidates and mode', (t) => {
+  t.deepEqual({
+    candidates:         [candidate, candidate],
+    index:              15,
+    separators:         [],
+    markedCandidateIds: {},
+    mode:               'some mode name',
+    scheme:             { enum: [] },
+  }, mapStateToProps({
+    popup: {
+      candidates: {
+        items: [candidate, candidate],
+        index: 15,
+      },
+      separators:         [],
+      markedCandidateIds: {},
+      mode:               'some mode name',
+      scheme:             {
+        enum: [],
+      },
+    },
+  }));
+});
+
+test('<HomePage /> | dispatch | dispatches SELECT_DANDIDATE on handleSelectCandidate', (t) => {
+  const dispatch = nisemono.func();
+  const payload = 'some payload data';
+
+  const handlers = mapDispatchToProps(dispatch);
+  handlers.handleSelectCandidate(payload);
+
+  t.true(dispatch.isCalled);
+  t.is(1, dispatch.calls.length);
+  const call = dispatch.calls[0];
+  t.is(1, call.args.length);
+  t.deepEqual({ type: 'SELECT_CANDIDATE', payload }, call.args[0]);
+});
+
+test('<HomePage /> | dispatch | dispatches QUERY on handleInputChange', (t) => {
+  const dispatch = nisemono.func();
+  const payload = 'some payload data';
+
+  const handlers = mapDispatchToProps(dispatch);
+  handlers.handleInputChange(payload);
+
+  t.true(dispatch.isCalled);
+  t.is(1, dispatch.calls.length);
+  const call = dispatch.calls[0];
+  t.is(1, call.args.length);
+  t.deepEqual({ type: 'QUERY', payload }, call.args[0]);
+});
+
+test('<HomePage /> | dispatch | dispatches POPUP_CLEANUP and POPUP_QUIT on dispatchQuit', (t) => {
+  const dispatch = nisemono.func();
+
+  const handlers = mapDispatchToProps(dispatch);
+  handlers.dispatchQuit();
+
+  t.true(dispatch.isCalled);
+  t.is(2, dispatch.calls.length);
+  const call1 = dispatch.calls[0];
+  t.is(1, call1.args.length);
+  t.deepEqual({ type: 'POPUP_CLEANUP' }, call1.args[0]);
+
+  const call2 = dispatch.calls[1];
+  t.is(1, call2.args.length);
+  t.deepEqual({ type: 'POPUP_QUIT' }, call2.args[0]);
+});
+
+test('<HomePage /> | dispatch | dispatches KEY_SEQUENCES for known keybinding in handleKeyDown', (t) => {
+  const dispatch = nisemono.func();
+  const SPC_KEY = 32;
+  const event = {
+    keyCode:        SPC_KEY,
+    ctrlKey:        true,
+    preventDefault: nisemono.func(),
+  };
+
+  const handlers = mapDispatchToProps(dispatch);
+  handlers.handleKeyDown(event);
+
+  t.true(dispatch.isCalled);
+  t.is(1, dispatch.calls.length);
+
+  const call = dispatch.calls[0];
+  t.is(1, call.args.length);
+  t.deepEqual({ type: 'KEY_SEQUENCE', payload: 'C-SPC' }, call.args[0]);
+
+  t.true(event.preventDefault.isCalled);
+});
+
+test('<HomePage /> | dispatch | doesn\'t dispatch KEY_SEQUENCES for unknown keybinding in handleKeyDown', (t) => {
+  const dispatch = nisemono.func();
+  const A_KEY = 65;
+  const event = {
+    keyCode:        A_KEY,
+    preventDefault: nisemono.func(),
+  };
+
+  const handlers = mapDispatchToProps(dispatch);
+  handlers.handleKeyDown(event);
+
+  t.false(dispatch.isCalled);
+  t.false(event.preventDefault.isCalled);
+});
