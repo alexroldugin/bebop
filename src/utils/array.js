5b32f8d5ddcf5a4df04116c836f3720b12e54f00
Alex Roldugin <2838600+alexroldugin@users.noreply.github.com>
2019-07-02 21:48:53 +0300

    added range array implementation

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,8 +1,9 @@
-export default function uncirculateIndex(items, index) {
+export function uncirculateIndex(items, index) {
   const len = items.length;
   return len > 1 ? ((index % len) + len) % len : 0;
 }
 
-export {
-  uncirculateIndex,
-};
+export const range = (start, stop, step = 1) => Array(Math.ceil((stop - start) / step))
+  .fill(start).map((x, y) => x + y * step);
+
+export default uncirculateIndex;
--- test/utils/array.test.js
+++ test/utils/array.test.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 import {
   uncirculateIndex,
+  range,
 } from '../../src/utils/array';
 
 function checkUncirculateIndex(t, { items, index }, expected) {
@@ -101,4 +102,57 @@ test(
   'uncirculateIndex | two elements with index=-6', checkUncirculateIndex,
   { items: ['item0', 'item1'], index: -6 },
   0,
+);
+
+function checkRange(t, { start, stop, step }, expected) {
+  t.deepEqual(range(start, stop, step), expected);
+}
+
+test(
+  'range | default step, [0, 5)', checkRange,
+  { start: 0, stop: 5 },
+  [0, 1, 2, 3, 4],
+);
+
+test(
+  'range | step=1, [0, 5)', checkRange,
+  { start: 0, stop: 5, step: 1 },
+  [0, 1, 2, 3, 4],
+);
+
+test(
+  'range | step=1, [3, 5)', checkRange,
+  { start: 3, stop: 5, step: 1 },
+  [3, 4],
+);
+
+test(
+  'range | step=1, [-3, 3)', checkRange,
+  { start: -3, stop: 3, step: 1 },
+  [-3, -2, -1, 0, 1, 2],
+);
+
+test(
+  'range | step=1, [-5, 3)', checkRange,
+  { start: -5, stop: -3, step: 1 },
+  [-5, -4],
+);
+
+
+test(
+  'range | step=2, [0, 10)', checkRange,
+  { start: 0, stop: 10, step: 2 },
+  [0, 2, 4, 6, 8],
+);
+
+test(
+  'range | step=2, [3, 10)', checkRange,
+  { start: 3, stop: 10, step: 2 },
+  [3, 5, 7, 9],
+);
+
+test(
+  'range | step=2, [-3, 2)', checkRange,
+  { start: -3, stop: 2, step: 2 },
+  [-3, -1, 1],
 );
